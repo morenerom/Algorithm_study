1b7a2e953424b777d8edc709007d126c760c343d
morenerom <audtmd5517@naver.com>
2018-09-22 00:05:17 +0900

    [Update] 1967

---
+++ BOJ[DFS,BFS]/1967.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <utility>
+using namespace std;
+
+bool visit[100001] = { false, };
+vector<pair<int, int> > vp[100001];
+vector<pair<int, int> > len;
+
+void DFS(int a, int l) {
+	int cnt = 0;
+	visit[a] = true;
+	for (int i = 0; i < vp[a].size(); i++) {
+		if (!visit[vp[a][i].first]) {
+			visit[vp[a][i].first] = true;
+			DFS(vp[a][i].first, l + vp[a][i].second);
+		}
+		else
+			++cnt;
+	}
+	if (cnt == vp[a].size())
+		len.push_back(make_pair(a, l));
+}
+
+bool cmp(pair<int, int> a, pair<int, int> b) { return a.second > b.second; }
+
+int main() {
+	int v, n, f, s;
+	cin >> v;
+	for (int i = 1; i <= v-1; i++) {
+		cin >> n >> f >> s;
+		vp[n].push_back(make_pair(f, s));
+		vp[f].push_back(make_pair(n, s));
+	}
+	DFS(1, 0);
+	sort(len.begin(), len.end(), cmp);
+	int ss = len.begin()->first;
+	memset(visit, 0, sizeof(visit));
+	len.clear();
+	DFS(ss, 0);
+	sort(len.begin(), len.end(), cmp);
+	cout << len.begin()->second << '\n';
+	return 0;
+}
